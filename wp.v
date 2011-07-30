4429cc3a70c872b5bc6fb4ba118802ac510ee522
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2011-07-30 13:39:14 +0400

    prove equivalence of adjointness law and traditional definition of wp

--- wp.v
+++ wp.v
@@ -2,9 +2,47 @@ Load basics.
 
 Variable wp : D -> D -> D.
 
-Axiom WP_definition:
+Definition WP_definition :=
   forall q r : D,
   [wp q r]q[r] /\
     forall p' : D, [p']q[r] -> p' ref wp q r.
 
 
+Definition AdjointnessLaw :=
+  forall p q r :D,
+  p ref wp q r <-> p;q ref r.
+
+
+Hint Unfold WP_definition AdjointnessLaw.
+
+Theorem ALWPD : AdjointnessLaw -> WP_definition.
+Proof.
+intro AL.
+autounfold.
+intuition.
+  apply AL.
+  trivial.
+  
+  apply AL.
+  trivial.
+Qed.
+
+Theorem WPDAL : WP_definition -> AdjointnessLaw.
+Proof.
+intro WPD.
+autounfold.
+intuition.
+apply Transitivity with (wp q r;q).
+intuition.
+apply WPD.
+apply WPD.
+trivial.
+Qed.
+
+
+
+(*
+Definition WP1 :=
+  forall p q r : D,
+  p ref wp q r -> 
+*)
